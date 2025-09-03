c6b5da12508217e686d475f738bf8e3280188dc5
Geraldine Ragsac <hello@geraldiner.com>
2025-09-03 13:38:30 -0400

    chore: Update site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import Header from "@/components/header";
 import "./styles/globals.css";
 
 export const metadata = {
-  title: "ACHN API 2.0 by Geraldine Ragsac",
-  description: "A reboot of the ACNH API originally created by Alexis Lours (@alexislours on GitHub).",
+  title: "Animal Crossing API by Geraldine Ragsac",
+  description: "A reboot of a free RESTful API for furniture, critters, fossils, art, music and villagers from Animal Crossing: New Horizons.",
 };
 
 export default function RootLayout({
